2ff4b15475cd076aee9572e3098b20f5757c39fc
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-03-15 16:00:38 +0100

    StringyNum empty-input-error handling implemented

--- StringyNum/index.js
+++ StringyNum/index.js
@@ -1,17 +1,30 @@
+'use strict';
+
+const info = document.querySelector('.info');
 const input = document.querySelector('#arrayInput');
 const convert = document.querySelector('button');
 const result = document.querySelector('.result');
 
 
+window.onload = function() {
+    input.value = '';
+}
+
 const arrayToObj = arr => {
+   
+    if (arr.value.length === 0 || arr.value === '') {
+        const error = document.createElement('p');
+        error.textContent = 'Please enter valid strings and numbers';
+        error.style.backgroundColor = 'orange';
+        error.style.color = 'red';
 
-    if (arr.length === 0) {
-        alert('Invalid Array');
-        throw new Error('Invalid Array');
+        info.appendChild(error);
+
+        throw new Error('Invalid input');
     }
-    
-    let array = arr.value.toString().split(' ').map(el => el > 0 ? Number(el) : el);
 
+    let array = arr.value.toString().split(' ').map(el => el > 0 ? Number(el) : el);
+  
 
     let str = array.filter(a => typeof a === 'string');
     let num = array.filter(a => typeof a === 'number');
@@ -29,6 +42,7 @@ const arrayToObj = arr => {
 convert.addEventListener('click', function () {
     arrayToObj(input);
     input.value = '';
+    input.focus();
 });
 
 // let array = ['bee', 6, 'better', 5, 'bayo', 4, 'beast', 3, 'ba', 8, 'b'];
