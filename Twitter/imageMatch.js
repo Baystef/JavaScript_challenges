267707b9e9c2984584c5b85a29ec077e96a72b22
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-03-26 18:01:03 +0100

    imageMatch.js is 80% complete; 80% because it fails in several test cases

--- Twitter/imageMatch.js
+++ Twitter/imageMatch.js
@@ -1,73 +1,82 @@
 function countMatches(grid1, grid2) {
-    let gridOne = grid1.filter(n => typeof(n) !== 'number');
-    let gridTwo = grid2.filter(n => typeof(n) !== 'number');
+    let gridOne = grid1.filter(n => typeof (n) !== 'number').map(n => n.split(''));
+    let gridTwo = grid2.filter(n => typeof (n) !== 'number').map(n => n.split(''));
+    
+    let islands = countIslands(gridOne);
+  
+    let matches = Array.from({
+        length: islands.count + 1
+    }).fill(true);
 
-    //this only gives number of connected region in each grid, but what i want to get is number of islands that match perfectly when the grids are overlayed.
-    return `We have ${countIslands(gridOne)} island(s) in grid 1 and ${countIslands(gridTwo)} island(s) in grid 2`
+    islands.images.forEach((a, x) => a.forEach((v, y) => matches[v] = matches[v] && gridTwo[x][y] && v));
+    
+    return matches.reduce((s, b) => s + !!b, 0);
 }
 
 function countIslands(grid) {
-    let markIsland = function (grid, x, y, visited) {
-        if (x < 0 || x > grid.length - 1 || y < 0 || y > grid[x].length - 1) return;
+    let markIsland = function (grid, x, y, count) {
 
+        if (grid[x] && grid[x][y] === -1) {
+            grid[x][y] = count;
 
-        if (visited[x][y] === true) return;
-        visited[x][y] = true;
-
-        if (grid[x][y] === '0') return;
-
-        markIsland(grid, x - 1, y, visited);
-        markIsland(grid, x + 1, y, visited);
-        markIsland(grid, x, y - 1, visited);
-        markIsland(grid, x, y + 1, visited);
+            markIsland(grid, x - 1, y, count);
+            markIsland(grid, x + 1, y, count);
+            markIsland(grid, x, y - 1, count);
+            markIsland(grid, x, y + 1, count);
+            return true;
+        }
     };
 
-    let visited = [];
-
-    for (let i = 0; i < grid.length; i++) {
-        visited[i] = [];
-    }
-
     let count = 0;
-
-    for (let x = 0; x < grid.length; x++) {
-        for (let y = 0; y < grid[x].length; y++) {
-            if (!visited[x][y] && grid[x][y] === '1') {
-                count++;
-                markIsland(grid, x, y, visited);
-            }
-            visited[x][y] = true;
-        }
-    }
+    let images = grid.map(a => a.map(v => -v));
     
-    return count;
+    images.forEach((a, x, aa) => a.forEach((_, y) => markIsland(aa, x, y, count) && count++));
+    return {
+        images,
+        count: count - 1
+    };
 }
 
 const sample1 = [
     4,
-    ['0', '1', '0', '0'],
-    ['1', '0', '0', '1'],
-    ['0', '0', '1', '1'],
-    ['0', '0', '1', '1']
+    '0100',
+    '1001',
+    '0011',
+    '0011'
 ]
 const sample2 = [
     4,
-    ['0', '1', '0', '1'],
-    ['1', '0', '0', '1'],
-    ['0', '0', '1', '1'],
-    ['0', '0', '1', '1']
+    '0101',
+    '1001',
+    '0011',
+    '0011'
 ]
 const sample3 = [
     3,
-    ['0', '0', '1'],
-    ['0', '1', '1'],
-    ['1', '0', '0']
+    '001',
+    '011',
+    '100'
 ]
 const sample4 = [
     3,
-    ['0', '0', '1'],
-    ['0', '1', '1'],
-    ['1', '0', '1', ]
+    '001',
+    '011',
+    '101',
+]
+const sample5 = [
+    4,
+    '0010',
+    '0111',
+    '0100',
+    '1111'
+]
+const sample6 = [
+    4,
+    '0010',
+    '0111',
+    '0110',
+    '1111'
 ]
 
-console.log(countMatches(sample1, sample2));
+
+console.log(countMatches(sample5, sample6));
