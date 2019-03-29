7b79f7b37f3ff2e0e77e257b0b97b97230707d99
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-03-30 00:45:37 +0100

    still on the Andela matter

--- Andela/Technical Challenge/concertSeat.js
+++ Andela/Technical Challenge/concertSeat.js
@@ -1,21 +1,26 @@
+const seats = document.querySelector('.seats');
+
 function conSeats(arrays) {
 
     for (let i = 0; i < arrays.length; i++) {
         let array = arrays[i];
         for (let j = 0; j < array.length; j++) {
-            // if (i === 0 && j === 0) console.log(`array[ij]: ${arrays[i][j]} and array[i+1,j]: ${arrays[i + 1][j]}`);
-            // if (i < 0 || i > arrays.length - 1 || j < 0 || j > array.length - 1) return;
-           
+
+            if (arrays[i][j] > arrays.length - 1 && arrays[i + 1][j] >= arrays.length - 1) return;
+
             if (arrays[i][j] < arrays[i + 1][j]) {
-                return true;
-                i++;
-                j++;
+
+                console.log('true');
+
             } else {
-                return false;
+                console.log(arrays[i][j]);
+                console.log('false');
+                // return false;
             }
-            // conSeats(arrays[i + 1][j]);
+
         }
     }
+
 }
 
 const arrays = [
@@ -28,7 +33,7 @@ const arrays = [
 ];
 
 const arrays2 = [
-    [1, 2, , 2, 1, 1],
+    [1, 2, 3, 2, 1, 1],
     [2, 4, 4, 3, 6, 2],
     [5, 5, 5, 5, 4, 4],
     [6, 6, 7, 6, 5, 5],
